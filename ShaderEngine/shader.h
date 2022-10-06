cbe800850271900ea426fa2ab4fe358258c9ea39
00DanielHarris <88143224+ZekDH@users.noreply.github.com>
2022-10-07 02:15:19 +1030

    Update shader.h
    Shader class with comments
    #ifndef used to make sure class is not initialized twice

--- ShaderEngine/shader.h
+++ ShaderEngine/shader.h
@@ -2,20 +2,22 @@
 #define SHADER_H
 #include <iostream>
 
+//String sources for each shader
 struct ShaderSource{
     std::string vertexShader;
     std::string fragmentShader;
 };
 
+//Implemented shader class
 class shader{
 
     public:
-        
         unsigned int shaderProgram;
         std::string filepath;
         ShaderSource parseShader(const std::string filepath);
         unsigned int initalizeShader(const std::string filepath);
 
+        //Shader initialzation function upon class creation
         shader(std::string inputFilepath){
             filepath = inputFilepath;
             shaderProgram = initalizeShader(filepath);
